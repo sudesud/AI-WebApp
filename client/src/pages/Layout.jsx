68da4b812fa2bedf5667cb27fae63fdbd7e9c8ee
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-09-15 21:08:03 +0300

    admin Layout ve sidebar baslandi

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { Menu, X } from 'lucide-react'
+import Sidebar from '../component/Sidebar'
 
 const Layout = () => {
 const navigate=useNavigate()
@@ -15,8 +16,14 @@ const [sidebar,setSidebar]=useState(false)
             :<Menu  onClick={()=>setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden'/>
         }
       </nav>
+      <div className='flex-1 w-full flex h-[calc(100vh-64px)]'>
+          <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
+          <div className='flex-1 bg-[#F4F7FB]'>
+            <Outlet/>
+          </div>
+      </div>
 
-      <Outlet/>
+      
     </div>
   )
 }
