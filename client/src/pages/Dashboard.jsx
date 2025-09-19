2ee015ea6e6ee5cc7da5a0d34946cdb89ed39605
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-09-19 12:14:56 +0300

    user dashboard baslandi

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Protect } from '@clerk/clerk-react'
+import {  Gem, Sparkles } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
+
+  const [creations,setCreations]=useState([])
+
+  const getDashboardData=async ()=>{
+      setCreations(dumm)
+  }
+
+  useEffect(()=>{
+    getDashboardData()
+  },[])
   return (
-    <div>
-      dashborad
+    <div className='h-full overflow-y-scroll p-6'>
+      <div className='flex justify-start gap-4 flex-wrap'>
+        {/* total creations card */}
+        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white
+        rounded-xl border border-gray-200'>
+              <div className='text-slate-600'>
+                <p className='text-sm'>Total Creations</p>
+                <h2 className='text-xl font-semibold'> {creations.length}</h2>
+              </div>
+              <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
+                <Sparkles className='w-5 text-white'/>
+              </div>
+        </div>
+              {/* active plan card */}
+         <div className='flex justify-between items-center w-72 p-4 px-6 bg-white
+        rounded-xl border border-gray-200'>
+              <div className='text-slate-600'>
+                <p className='text-sm'>Active Plan</p>
+                <h2 className='text-xl font-semibold'> <Protect plan='premium' fallback='Free'>Premium</Protect></h2>
+              </div>
+              <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
+                <Gem className='w-5 text-white'/>
+              </div>
+        </div>
+      </div>
     </div>
   )
 }
