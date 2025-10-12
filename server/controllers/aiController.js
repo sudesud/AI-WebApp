62d15f8daa2372d348821f2a3ed22d99a44a9265
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-10-12 18:17:34 +0300

    aiConrtoller yazildi

---
+++ server/controllers/aiController.js
@@ -0,0 +1,46 @@
+import { OpenAI } from 'openai'
+import sql from '../configs/db.js';
+
+const AI =  new OpenAI(
+    api_key=process.env.OPENAI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+export const generateArticle=async(req,res)=>{
+    try {
+        const {userId}=req.auth();
+        const {prompt,lenght}=req.body;
+        const {plan}=req.plan;
+        const free_usage=req.free_usage;
+        
+        if(plan!=='premium' && free_usage>=10){
+            return res.json({success:false,message:'You have reached your limit of free usage'})
+
+        }
+        const response = await AI.chat.completions.create({
+    model: "gemini-2.0-flash",
+    messages: [
+        {
+            role: "user",
+            content: prompt,
+        },
+    ],
+    temperature:0.7,
+    max_tokens:lenght,
+});
+    const content =response.choices[0].message.content;
+    await sql `INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},${prompt},${content},'article')`
+
+    if(plan!=='premium'){
+        await clerkClient.users.updateUserMetadata(userId,{
+            privateMetadata:{
+                free_usage:free_usage+1
+            }
+        })
+    }
+    res.json({success:true,message:content})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
+}
