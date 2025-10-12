2a8b0f31ba777449ed23a8ccc94d97e83bfd4f9c
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-10-12 21:58:15 +0300

    aiController devam

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -121,3 +121,67 @@ export const generateImage=async(req,res)=>{
         res.json({success:false,message:error.message})
     }
 }
+
+
+export const removeImageBackground=async(req,res)=>{
+    try {
+        const {userId}=req.auth();
+        const {image}=req.file;
+        const plan=req.plan;
+        
+        
+        if(plan!=='premium' ){
+            return res.json({success:false,message:'This feature is only available for premium users'})
+
+        }
+      
+      
+   const{secure_url} =await cloudinary.uploader.upload(image.path,{
+    transformation:[
+        {
+            effect:"background_removal",
+            background_removal:"remove_the_background"
+        }
+    ]
+   })
+
+
+    await sql `INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},'Remove background from image',${secure_url},'image')`
+
+    
+    res.json({success:true,content:secure_url})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export const removeImageObject=async(req,res)=>{
+    try {
+        const {userId}=req.auth();
+        const {object}=req.body;
+        const {image}=req.file;
+        const plan=req.plan;
+        
+        
+        if(plan!=='premium' ){
+            return res.json({success:false,message:'This feature is only available for premium users'})
+
+        }
+      
+      
+   const{public_id} =await cloudinary.uploader.upload(image.path)
+   const imageUrl=  cloudinary.url(public_id,{
+    transformation:[{effect:`gen_remove:${object}`}],
+    resource_type:'image'
+   })
+
+    await sql `INSERT INTO creations (user_id,prompt,content,type) VALUES (${userId},${`Removed ${object} from image}`},${imageUrl},'image')`
+
+    
+    res.json({success:true,content:imageUrl})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message})
+    }
+}
