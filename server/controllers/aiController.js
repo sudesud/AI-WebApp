60c00010675b5e6b68494f402e7422c8c218a07e
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-10-16 11:40:04 +0300

    RemoveObject frontend ve baçkend birleştirilmeye baslandi

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -211,7 +211,7 @@ export const removeImageObject=async(req,res)=>{
     try {
         const {userId}=req.auth();
         const {object}=req.body;
-        const {image}=req.file;
+        const image=req.file;
         const plan=req.plan;
         
         
